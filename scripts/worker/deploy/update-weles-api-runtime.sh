3f67ed033cc88528e2dfada155f63a7fadb64c95
lbartoszcze <lgb2127@columbia.edu>
2026-08-12 11:38:11 -0700

    Pin Weles after Funnel cutover

--- scripts/worker/deploy/update-weles-api-runtime.sh
+++ scripts/worker/deploy/update-weles-api-runtime.sh
@@ -2,7 +2,7 @@
 # Build the Weles API runtime from one exact reviewed repository revision.
 set -eu
 
-REVISION='439eb7aa69a56d2ed805978677dfdb35ffa2af4f'
+REVISION='eb4f2a055d161e8abbe226a060b4d442b6e50c69'
 REPOSITORY='https://github.com/wisent-ai/weles.git'
 WORK="$HOME/.stado/build-work/weles-api-managed"
 PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
