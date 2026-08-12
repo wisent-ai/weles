b48c8ff64e9bddc6bd7f3e96897337d2c675aa1f
lbartoszcze <lgb2127@columbia.edu>
2026-08-12 01:18:52 -0700

    Pin repaired Weles API runtime

--- scripts/worker/deploy/update-weles-api-runtime.sh
+++ scripts/worker/deploy/update-weles-api-runtime.sh
@@ -2,7 +2,7 @@
 # Build the Weles API runtime from one exact reviewed repository revision.
 set -eu
 
-REVISION='4c53a0e0b8b115a4aad7e2f268ea0af7e80e1382'
+REVISION='d06dec2b4e74ca2b45114540589fcb170bb2b778'
 REPOSITORY='https://github.com/wisent-ai/weles.git'
 WORK="$HOME/.stado/build-work/weles-api-managed"
 PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
