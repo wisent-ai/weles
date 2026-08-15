1ea4ee80c16db5dc78249745a5f0a378a88d4d28
lbartoszcze <lgb2127@columbia.edu>
2026-08-14 19:34:18 -0700

    Pin the managed runtime to the revision that can read the vault

--- scripts/worker/deploy/update-weles-api-runtime.sh
+++ scripts/worker/deploy/update-weles-api-runtime.sh
@@ -2,7 +2,7 @@
 # Build the Weles API runtime from one exact reviewed repository revision.
 set -eu
 
-REVISION='bacbc6df3450ad638ebfb779ec6d84d67b45e04d'
+REVISION='39ca0b82b6b1516649706a29fd872a15f15d9ba4'
 REPOSITORY='https://github.com/wisent-ai/weles.git'
 WORK="$HOME/.stado/build-work/weles-api-managed"
 PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
