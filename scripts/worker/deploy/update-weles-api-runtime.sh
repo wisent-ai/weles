a1c6a49d71669ce34d40bc609c461ea2f5fe4f46
lbartoszcze <lgb2127@columbia.edu>
2026-08-17 17:16:42 -0700

    Build the runtime that can read a login seed

--- scripts/worker/deploy/update-weles-api-runtime.sh
+++ scripts/worker/deploy/update-weles-api-runtime.sh
@@ -2,7 +2,7 @@
 # Build the Weles API runtime from one exact reviewed repository revision.
 set -eu
 
-REVISION='4a2408d69c966467cebc24e718f33a3c21e27b29'
+REVISION='6f270c01862bdde64d3026694dc47ab29962fca4'
 REPOSITORY='https://github.com/wisent-ai/weles.git'
 WORK="$HOME/.stado/build-work/weles-api-managed"
 PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
