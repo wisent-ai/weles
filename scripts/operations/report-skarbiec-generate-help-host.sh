8ca08d5eeed68c2d61ba40ec73cc9e1c3405afb2
lbartoszcze <lgb2127@columbia.edu>
2026-09-05 12:49:19 -0700

    Remove completed Figma investigation and manual export helpers

--- scripts/operations/report-skarbiec-generate-help-host.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-export SKARBIEC_VAULT_FILE="$HOME/.stado/skarbiec.vault.json"
-skarbiec=$(/opt/homebrew/bin/node "$HOME/weles/scripts/_shared/skarbiec-runtime.mjs" active-binary)
-exec "$skarbiec" set-json --help
